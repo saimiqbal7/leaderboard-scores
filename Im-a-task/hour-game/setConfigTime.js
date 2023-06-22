405d97a85e83be9b97bc27f94ee8145ebf79f104
Yigit Dincsoy <w0471994@campus.nscc.ca>
2023-06-22 12:20:32 -0300

    Added UNIX Epoch Time

--- Im-a-task/hour-game/setConfigTime.js
+++ Im-a-task/hour-game/setConfigTime.js
@@ -1,13 +1,18 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 async function setConfigTime() {
-  const uri = 'mongodb+srv://koii:KoiiToTheMoon@im-a-task.b2xhqzs.mongodb.net/?retryWrites=true&w=majority';
+  const uri =
+    "mongodb+srv://koii:KoiiToTheMoon@im-a-task.b2xhqzs.mongodb.net/?retryWrites=true&w=majority";
   const client = new MongoClient(uri);
   await client.connect();
-  const database = client.db('games');
-  const config = database.collection('config');
+  const database = client.db("games");
+  const config = database.collection("config");
 
-  await config.insertOne({ _id: "time", value: 600000 }); // 10 minutes
+  setInterval(async () => {
+    const currentTime = Math.floor(Date.now() / 1000); // Unix epoch time in seconds
+    await config.updateOne({ _id: "time" }, { $set: { value: currentTime } });
+    console.log("Time updated:", currentTime);
+  }, 1000); // Update every second
 }
 
 setConfigTime().catch(console.error);
